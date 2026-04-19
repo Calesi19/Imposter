cd675f31a38badba101ed6544e395838b9cc1dd2
Carlos Lespin <carlos.lespin.silva@gmail.com>
2026-04-19 14:44:15 -0400

    fix: start path

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/Imposter/',
+        scope: '/Imposter/',
         icons: [
           {
             src: 'icons/icon-192.png',
